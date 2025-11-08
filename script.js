68969b14989a64da9317369162b672a08bff0355
AltSystem42 <50642697+AltSystem42@users.noreply.github.com>
2025-11-08 11:02:40 -0400

    added function changeBackgroundColor of a box

--- script.js
+++ script.js
@@ -9,10 +9,18 @@ for(let j = 0; j < boxCountColumn; j++){
     body.appendChild(column)
     for(let i = 0; i < boxCountRow; i++){
         const box = document.createElement("div")
-        box.classList.add(`boxRow${i}${j}`)
+        box.classList.add(`box${i}${j}`)
         box.classList.add("box")
         column.appendChild(box)
         console.log(i)
     }
     console.log(j)
 }
+
+// get player input from arrow keys to either change the background color of boxXY
+function changeBackGround(x, y) {
+    const box = document.querySelector(`.box${x}${y}`);
+    box.style.backgroundColor = "black" 
+
+}
+
