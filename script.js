10b3cc2d2f2efb2f2fa7ba359f5fbf47303e2f3d
AltSystem42 <50642697+AltSystem42@users.noreply.github.com>
2025-11-08 14:02:47 -0400

    added reset button and change size button
    added reset button
    added change size button

--- script.js
+++ script.js
@@ -1,31 +1,62 @@
 const container = document.querySelector('.container')
-const boxCountRow = 16
-const boxCountColumn = 16
+const changeGridSize = document.querySelector(`.changegridsize`)
+const reset = document.querySelector(".reset")
+container.style.maxWidth = "400px"
+let gridSize = 100
+buildGrid(gridSize)
 
-for(let j = 0; j < boxCountColumn; j++){
-    const column = document.createElement(`div`)
-    column.classList.add(`column${j}`)
-    column.classList.add('column')
-    container.appendChild(column)
-    for(let i = 0; i < boxCountRow; i++){
-        const box = document.createElement("div")
-        box.classList.add(`box${i}${j}`)
-        box.classList.add("box")
-        column.appendChild(box)
+function buildGrid(boxCount) {
+  const fragment = document.createDocumentFragment()
+  for (let j = 0; j < boxCount; j++) {
+    const column = document.createElement('div')
+    column.classList.add(`column${j}`, 'column')
+
+    for (let i = 0; i < boxCount; i++) {
+      const box = document.createElement('div')
+      box.classList.add(`box${i}${j}`, 'box')
+      column.appendChild(box)
     }
+
+    fragment.appendChild(column)
+  }
+
+  container.innerHTML = '' // clear old grid
+  container.appendChild(fragment)
 }
 
-// get player input from mouse over
-document.querySelectorAll('.box').forEach(box => {
-    box.addEventListener('mouseenter', () => {
-        changeBackGround(box)
-    })
+//reset the grid and put in existing gridSize
+function resetGrid(currentBoxCount){
+    buildGrid(currentBoxCount)
+}
+
+//button to choose what size of grid you want
+changeGridSize.addEventListener('click', () => {
+    
+    gridSize = parseInt(prompt('Size of new grid'))
+    console.log(gridSize)
+    if (gridSize > 100 || isNaN(gridSize)){
+        alert("Error too big")
+    } else {
+    let px = (gridSize * 4) + "px"
+    container.style.maxWidth = px;
+    buildGrid(gridSize)
+    }
+})
+
+//reset event listener button
+reset.addEventListener('click', () => {
+    buildGrid(gridSize)
+})
+
+//mouseover event for changing background color of boxes
+container.addEventListener('mouseover', (e) => {
+  if (e.target.classList.contains('box')) {
+    changeBackGround(e.target)
+  }
 })
 
 
 //  change background color of a box
 function changeBackGround(boxElement) {
     boxElement.style.backgroundColor = "black" 
-
 }
-
