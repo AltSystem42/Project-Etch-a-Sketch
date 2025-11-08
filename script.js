c7a83a09ccab172f71191736fe8bd2c13eecfb19
AltSystem42 <50642697+AltSystem42@users.noreply.github.com>
2025-11-08 11:45:37 -0400

    get player input from mouseenter for each box

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const body = document.querySelector("body")
+const container = document.querySelector('.container')
 const boxCountRow = 16
 const boxCountColumn = 16
 
@@ -6,21 +6,26 @@ for(let j = 0; j < boxCountColumn; j++){
     const column = document.createElement(`div`)
     column.classList.add(`column${j}`)
     column.classList.add('column')
-    body.appendChild(column)
+    container.appendChild(column)
     for(let i = 0; i < boxCountRow; i++){
         const box = document.createElement("div")
         box.classList.add(`box${i}${j}`)
         box.classList.add("box")
         column.appendChild(box)
-        console.log(i)
     }
-    console.log(j)
 }
 
-// get player input from arrow keys to either change the background color of boxXY
-function changeBackGround(x, y) {
-    const box = document.querySelector(`.box${x}${y}`);
-    box.style.backgroundColor = "black" 
+// get player input from mouse over
+document.querySelectorAll('.box').forEach(box => {
+    box.addEventListener('mouseenter', () => {
+        changeBackGround(box)
+    })
+})
+
+
+//  change background color of a box
+function changeBackGround(boxElement) {
+    boxElement.style.backgroundColor = "black" 
 
 }
 
